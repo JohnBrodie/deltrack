a913bcafd372429615529163e1f017ab6219c652
John Brodie <jdb356@drexel.edu>
2012-03-07 21:21:44 -0500

    Clean up the code a bit

--- deltrack.py
+++ deltrack.py
@@ -8,18 +8,22 @@ import dbus
 import sys
 import subprocess
 import os
+
 from urlparse import urlparse
 from urllib import unquote
-from pprint import pprint
 
-save = [".mp3",".flac",".wma",".ogg"] # Don't delete dir if it contains any of these files.
-exts = [".tqd"] # Other extensions to delete with same base name.
+
+# Don't delete dir if it contains any of these files.
+save = [".mp3", ".flac", ".wma", ".ogg"]
+# Other extensions to delete with same base name.
+exts = [".tqd"]
 
 try:
     bus = dbus.SessionBus()
 except:
     print "Could not connect to dbus."
     sys.exit(1)
+
 try:
     player = dbus.Interface(bus.get_object('org.mpris.amarok', '/Player'),
               dbus_interface='org.freedesktop.MediaPlayer')
@@ -53,32 +57,35 @@ else:
 
 path = ''
 for ext in exts: #Delete each basename+extension.
-    loc = ''.join([basename,ext])
+    loc = ''.join([basename, ext])
     cmdext = ['kioclient', 'move', loc, 'trash:/']
-    print "Running %s"%' '.join(cmdext)
+    print "Running %s" % ' '.join(cmdext)
     retcodext = subprocess.call(cmdext)
     path = urlparse(loc).path
-    if retcodext == 0:    
-        print 'Successfully trashed "%s"'%path
-    else:
-        print 'Could not trash"%s"'%path
 
-direc = os.path.split(path)[0] #If the dir is empty let's get rid of it, as well.
-subdir = '' # We'll set this below, if it exists.
-direc = unquote(direc) # Replace KDE's sillyness.
-#The following for loops seem a bit ugly, but this seems quicker than using recursion and ending up many levels deep,
-#and the various exits are off-putting, but we want to exit asap if we can.
+    if retcodext == 0:
+        print 'Successfully trashed "%s"' % path
+    else:
+        print 'Could not trash"%s"' % path
+
+#If the dir is empty let's get rid of it, as well.
+direc = os.path.split(path)[0]
+direc = unquote(direc)
+
 for f in os.listdir(direc) :
-    if os.path.isdir(os.path.join(direc, f)) : # We'll go one level deep, no more, takes time.
+    if os.path.isdir(os.path.join(direc, f)):
         subdir = os.path.join(direc, f)
         subls = os.listdir(subdir)
+
         if len(subls) > 15 : # We probably don't want to delete this.
             sys.exit(0)
+
         for s in subls :
-            if os.path.isdir(os.path.join(subdir, s)) : # Only one level deep, so stop here.
+            if os.path.isdir(os.path.join(subdir, s)):
                 sys.exit(0)
             if os.path.splitext(s)[1] in save :
                 sys.exit(0)
+
     if os.path.splitext(f)[1] in save :
         sys.exit(0)
 
